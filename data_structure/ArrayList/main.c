48e56663844bd7a02548286f8a87877bc48e11eb
hedoel <hedroel@163.com>
2026-07-11 19:44:28 +0800

    feat:测试程序

--- data_structure/ArrayList/main.c
+++ data_structure/ArrayList/main.c
@@ -1,7 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+
+int add(int x, int y)
+{
+	return x + y;
+}
 int main()
 {
-
+	int x = 0; int y = 0; int c = 0;
+	if (scanf("%d %d", &x, &y) != 2)
+	{
+		printf("输入失败");
+	}
+	c = add(x, y);
+	printf("结果为%d", c);
 	return 0;
 }
